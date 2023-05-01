4dc86976a52278e3422b8acb1ac502bdda28dd6a
Saurav Mehto <sauravmehto.98@gmal.com>
2023-05-02 02:47:29 +0530

    ipod js
    ipod js

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./wallpaper1.gif";
+import wallpaper2 from "./wallpaper2.gif";
+import wallpaper3 from "./wallpaper3.gif";
+import wallpaper4 from "./wallpaper4.gif";
+import wallpaper5 from "./wallpaper5.gif";
+import coverflow from "./coverflow.png";
+import games from "./games.gif";
+import allsongs from "./allsongs.gif";
+import artists from "./artists.gif";
+import albums from "./albums.gif";
+import song1Img from "./stay.png";
+import song2Img from "./deserve-you.jpg";
+import song3Img from "./yummy.png";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
